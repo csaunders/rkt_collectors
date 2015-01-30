cdc6f392e850457faeb9792ff94a6f5c20477f66
Chris Saunders <chris.saunders@shopify.com>
2015-01-30 15:28:24 +0000

    Moved tests over to a different file
    This is to reduce all the noise in the actual
    collector file and make it easier to read.
    
    Also hacks around an issue where roots are just
    numbers in test but they are root? types in the
    real world. I'll look around for a correct way
    to do this but I'm not super sure at the moment.

--- my_simple_collector.rkt
+++ my_simple_collector.rkt
@@ -1,29 +1,31 @@
 #lang plai/collector
+(provide
+ heap-ptr
+ heap-ptr-force!
+ set-heap-ptr!
+ incr-heap!
+ free-memory
+ slot-of-size
+ find-space-for
+ allocate-cons
+ mark
+ perform-mark
+ sweep)
+
 (define heap-ptr 'uninitialized-heap-ptr)
 
-(define (assert-fail success-message failure-message fn)
-  (with-handlers ([exn:fail? (lambda (exn) success-message)])
-    (fn)
-    failure-message))
+(define (heap-ptr-force! address)
+  (set! heap-ptr address))
 
 (define (set-heap-ptr! pos)
   (if (eq? 'free (heap-ref pos))
       (set! heap-ptr pos)
       (set-heap-ptr! (+ 1 pos))))
 
-(test ;; That set-heap-ptr sets the pointer to the position of the first free entry
- (with-heap (vector 1 2 3 'free 'free 'free) (set-heap-ptr! 0)
-            heap-ptr)
- 3)
-
 (define (free-memory)
   (filter-map (lambda (addr)
                 (and (eq? 'free (heap-ref addr)) addr))
               (build-list (heap-size) values)))
-
-(test ;; finding free memory
- (with-heap (vector 'x 'x 'x 'free 'x) (free-memory))
- '(3))
 
 (define (oom? offset)
   (when (> offset 60) (write "point of interest"))
@@ -59,32 +61,12 @@
                  (slot-of-size size (cdr memory-space))))]
           [else (slot-of-size size (cdr memory-space))]))))
 
-(test ;; slot-of-size when there is free space but not consecutively
- (let ([memory '(0 2 4 6 8)])
-   (slot-of-size 2 memory))
- empty)
-
-(test ;; slot-of-size when there is not just enough free consecutive space
- (let ([memory '(0 1 2 4 5)])
-   (slot-of-size 4 memory))
- empty)
-
-(test ;; slot-of-size when there is enough free consecutive space
- (let ([memory '(0 1 2 3 4 5)])
-   (slot-of-size 4 memory))
- '(0 1 2 3))
-
-(test ;; slot-of-size when the free memory is not right at the start
- (let ([memory '(0 1 3 4 6 7 8 9)])
-   (slot-of-size 4 memory))
- '(6 7 8 9))
-
 (define (find-space-for type get-roots)
   (let* ([space (free-memory)]
          [address-finder (lambda ()
                             (cond
-                              [(eq? 'prim type) (slot-of-size 2 (free-memory))]
-                              [(eq? 'cons type) (slot-of-size 3 (free-memory))]))]
+                              [(eq? 'prim type) (slot-of-size 2 space)]
+                              [(eq? 'cons type) (slot-of-size 3 space)]))]
          [available-addresses (address-finder)])
     (if (empty? available-addresses)
         (begin
@@ -95,29 +77,6 @@
                 new-available-addresses)))
         available-addresses)))
 
-(test ;; finding available addresses for cons cells
- (let ([v (make-vector 12 'free)])
-   (with-heap v (find-space-for 'cons (lambda () '()))))
- '(0 1 2))
-
-(test ;; finding available addresses for flat data
- (let ([v (make-vector 12 'free)])
-   (with-heap v (find-space-for 'prim (lambda () '()))))
- '(0 1))
-
-(test ;; finding available addresses for memory that is very fragmented
- (let ([v (vector 'x 'x 'free 'x 'free 'x 'x 'free 'free)])
-   (with-heap v (find-space-for 'prim (lambda () '()))))
- '(7 8))
-
-(test ;; throws an error when there is not enough space to fit a item
- (assert-fail
-  "error raised"
-  "error not raised"
-  (lambda ()
-    (let ([v (vector 'x 'x 'free 'x 'free 'x 'x 'free 'free)])
-      (with-heap v (find-space-for 'cons (lambda () '()))))))
- "error raised")
 
 (define (memory-addresses item)
   (let ((value (if (number? item)(heap-ref item) #f)))
@@ -136,25 +95,11 @@
       (sweep (cdr addresses))
       addresses)))
 
-(test
- (let [(v (vector 0 1 2 3 4 5 6))]
-   (with-heap v
-            (sweep '(0 3 4 6))
-            v))
- (vector 'free 1 2 'free 'free 5 'free))
-
-;; Allocator Initialization
-
 (define (init-allocator)
   (let ([memory (build-list (heap-size) values)])
     (begin
       (sweep memory)
       (set! heap-ptr 0))))
-
-(test (let ([v (make-vector 12 'x)])
-        (with-heap v (init-allocator))
-        v)
-      (make-vector 12 'free))
 
 ;; Flat Value Allocation
 
@@ -188,33 +133,6 @@
       )
     cons-ptr))
 
-(test ;;allocating a cons cell
- (let ([v (make-vector 4 'free)])
-   (with-heap v
-              (begin ;; Because we are modifying global variables we need to fix the heap ptr
-               (init-allocator)
-               (allocate-cons 8 2)))
-   v)
- (vector 'cons 8 2 'free))
-
-(test ;;allocating a cons cell correctly increments the heap-ptr
- (let ([v (make-vector 4 'free)])
-   (with-heap v
-              (begin ;; Because we are modifying global variables we need to fix the heap ptr
-                (init-allocator)
-                (allocate-cons 8 2)))
-   heap-ptr)
- 3)
-
-(test ;;allocating a cons cell returns the address of the 'cons
- (let ([v (make-vector 8 'free)])
-   (with-heap v
-              (begin
-                (init-allocator)
-                (allocate-cons 8 2)
-                (allocate-cons 8 3))))
- 3)
-
 (define (gc:cons f r)
   (when (oom? 3)
     (sweep (mark)))
@@ -223,16 +141,6 @@
 (define (gc:cons? a)
  (eq? (heap-ref a) 'cons))
 
-(test
- (with-heap (vector 'free 'free 'prim 2 'cons 2 7 'prim 3)
-            (gc:cons? 4))
- #t)
-
-(test
- (with-heap (vector 'free 'free 'prim 2 'cons 2 7 'prim 3)
-            (gc:cons? 2))
- #f)
-
 (define (raise-if-not-cons a)
   (when (not (gc:cons? a))
     (error 'gc:first "expects address of cons")))
@@ -240,39 +148,10 @@
 (define (gc:first a)
   (raise-if-not-cons a)
   (heap-ref (+ 1 a)))
-
-(test ;; gc:first for a heap ref to a actual cons-cell
- (with-heap (vector 'prim 1 'prim 2 'cons 0 2)
-            (gc:first 4))
- 0)
-
-(test ;; gc:first for a heap-ref that is not a cons cell
- (assert-fail
-  "error raised"
-  "error not raised"
-  (lambda ()
-    (with-heap (vector 'prim 1 'cons 0 1)
-              (gc:first 0))))
- "error raised")
  
 (define (gc:rest a)
   (raise-if-not-cons a)
   (heap-ref (+ 2 a)))
-
-(test ;; gc:rest for a heap-ref to an actual cons-cell
- (with-heap
-  (vector 'prim 1 'prim 2 'cons 0 2)
-  (gc:rest 4))
- 2)
-
-(test ;; gc:rest for a heap-ref to an invalid cons-cell
- (assert-fail
-  "error raised"
-  "error not raised"
-  (lambda ()
-    (with-heap (vector 'prim 1 'cons 0 1)
-               (gc:rest 0))))
- "error raised")
   
 (define (gc:set-first! a f)
   (raise-if-not-cons a)
@@ -289,8 +168,15 @@
  (heap-ref (+ 1 a)))
  
 ;; Actual Garbage Collection Business
-(define (mark get-roots)
-  (let* ([greys (map read-root (get-roots))]
+(define (mark . get-roots)
+  (let* ([roots (if (not (eq? '() get-roots)) (get-roots) (get-root-set))]
+         [greys (map
+                 (lambda (root)
+                   ;; Hack -- How do I create roots when testing?
+                   (if (root? root)
+                       (read-root root)
+                       root))
+                 roots)]
          [whites (remv* greys (build-list (heap-size) values))])
     (perform-mark '() greys whites)))
 
@@ -302,51 +188,3 @@
              [new-blacks (append blacks (list item))]
              [new-greys (append (cdr greys) (cdr addresses))])
         (perform-mark new-blacks new-greys whites))))
-
-(test ;; that calling reachable-from-root returns true if the address can be found in the set
- (with-heap (vector 'prim 1 'prim 2 'cons 0 2 'prim 3 'free)
-            (with-roots '(4) (mark)))
- '(7 8 9))
-  
-(test ;; that calling mark when there are no roots returns all the memory addresses
- (with-heap (make-vector 4)
-            (with-roots '()
-                        (mark)))
- (list 0 1 2 3))
-
-(test ;; that calling mark when there are live children does not include those addresses
- (with-heap
-        ;chld   ;chld   ;root     ;waste
-  (vector 'prim 1 'prim 2 'cons 0 2 'prim 4)
-  (with-roots '(4) (mark)))
- (list 7 8))
-
-(test ;; that calling mark when there is a longer collection of cons cells
- (with-heap
-  (vector 'cons 9 3 'cons 11 6 'cons 13 15 'prim 1 'prim 2 'prim 3 'prim empty)
-  (with-roots '(0) (mark)))
- '())
-
-(test ;; that calling incr-heap! with no arguments increases the heap by 1
- (begin
-   (set! heap-ptr 0)
-   (incr-heap!))
- 1)
-
-(test ;; that calling incr-heap! with a single argument increments the heap by that amount
- (begin
-   (set! heap-ptr 0)
-   (incr-heap! 5))
- 5)
-
-(test ;; that calling incr-heap! with a non-zero starting value returns heap end location
- (begin
-   (set! heap-ptr 4)
-   (incr-heap! 2))
- 6)
-
-(test ;; that calling incr-heap! with multiple arguments only uses the first
- (begin 
-   (set! heap-ptr 1)
-   (incr-heap! 4 5 6 7))
- 5)
